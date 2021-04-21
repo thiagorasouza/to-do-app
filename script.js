572f28019e1ce7b65fd7a61b5be4852e780b93a8
Thiago Souza <thiagorasouza@gmail.com>
2021-04-20 23:02:48 -0300

    User can remove tasks

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-// let itemNum = 1;
-
+// ================
+// EVENT ATTACHMENT
+// ================
 const taskList = document.querySelector('.scn-listing ul');
 
 const newTaskInput = document.getElementById('inp-new-task');
@@ -12,10 +13,9 @@ newTaskInput.addEventListener('keyup', e => {
 const newTaskBtn = document.querySelector('.btn-new-task');
 newTaskBtn.addEventListener('click', () => insertTask(newTaskInput.value));
 
-function getLastNum() {
-  let lastChild = taskList.lastElementChild;
-  return lastChild ? lastChild.dataset.id : 0;
-}
+// ============
+// USER ACTIONS
+// ============
 
 function insertTask(desc) {
   const task = document.createElement('li');
@@ -23,17 +23,39 @@ function insertTask(desc) {
   let taskNum = Number(getLastNum()) + 1;
   task.dataset.id = taskNum;
 
+  let idf = 'task-' + taskNum;
+
   task.innerHTML = `
       <input type="checkbox" name="task-${taskNum}" id="task-${taskNum}">
       <label for="task-${taskNum}">${desc}</label>
-      <button type="button">-</button>
+      <button type="button" data-id="${taskNum}">-</button>
   `;
 
-  task.querySelector('input[type="checkbox"]').addEventListener('change', toggleTask);
+  task.querySelector('input[type="checkbox"]').addEventListener('change', () => toggleTask(task));
+  task.querySelector('button').addEventListener('click', () => deleteTask(task));
 
   taskList.appendChild(task);
 }
 
-function toggleTask(e) {
-    e.target.parentElement.classList.toggle('complete');
+function toggleTask(task) {
+  task.classList.toggle('complete');
 }
+
+function deleteTask(task) {
+  task.remove();
+}
+
+// ================
+// HELPER FUNCTIONS
+// ================
+function getLastNum() {
+  let lastChild = taskList.lastElementChild;
+  return lastChild ? lastChild.dataset.id : 0;
+}
+
+// ================
+// INITIALIZATION
+// ================
+insertTask('Task 1');
+insertTask('Task 2');
+insertTask('Task 3');
