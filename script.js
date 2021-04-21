cdda340c7d700e4dbb62505fb22dd1beac9b821a
Thiago Souza <thiagorasouza@gmail.com>
2021-04-21 18:20:19 -0300

    User can edit a task

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
 const taskList = document.querySelector('.scn-listing ul');
 
 const newTaskInput = document.getElementById('inp-new-task');
-newTaskInput.addEventListener('keyup', e => {
-  if (e.key === 'Enter') {
-    insertTask(e.target.value);
+newTaskInput.addEventListener('keyup', event => {
+  if (event.key === 'Enter') {
+    insertTask(event.target.value);
   }
 });
 
@@ -23,17 +23,23 @@ function insertTask(desc) {
   let taskNum = Number(getLastNum()) + 1;
   task.dataset.id = taskNum;
 
-  let idf = 'task-' + taskNum;
-
   task.innerHTML = `
-      <input type="checkbox" name="task-${taskNum}" id="task-${taskNum}">
-      <label for="task-${taskNum}">${desc}</label>
-      <button type="button" data-id="${taskNum}">-</button>
+      <input type="checkbox" name="box-task-${taskNum}" id="box-task-${taskNum}">
+      <label for="box-task-${taskNum}">${desc}</label>
+      <input type="text" name="inp-task-${taskNum}" value="${desc}" hidden>
+      <button type="button" class="btn-edit-task">E</button>
+      <button type="button" class="btn-del-task">D</button>
   `;
 
   task.querySelector('input[type="checkbox"]').addEventListener('change', () => toggleTask(task));
-  task.querySelector('button').addEventListener('click', () => deleteTask(task));
-
+  task.querySelector('.btn-del-task').addEventListener('click', () => deleteTask(task));
+  task.querySelector('.btn-edit-task').addEventListener('click', () => toggleEditInput(task));
+  task.querySelector('input[type="text"]').addEventListener('keyup', event => {
+    if (event.key === 'Enter') {
+      editTask(task, event.target.value);
+    }
+  });
+  
   taskList.appendChild(task);
 }
 
@@ -45,6 +51,26 @@ function deleteTask(task) {
   task.remove();
 }
 
+function toggleEditInput(task) {
+  let label = task.querySelector('label');
+  let input = task.querySelector('input[type="text"]');
+  
+  toggleElement(label);
+  toggleElement(input);
+}
+
+function editTask(task, desc) {
+  let label = task.querySelector('label');
+  let input = task.querySelector('input[type="text"]');
+
+  label.textContent = desc;
+
+  toggleElement(label);
+  toggleElement(input);
+
+  input.value = desc;
+}
+
 // ================
 // HELPER FUNCTIONS
 // ================
@@ -52,6 +78,18 @@ function getLastNum() {
   let lastChild = taskList.lastElementChild;
   return lastChild ? lastChild.dataset.id : 0;
 }
+
+function toggleElement(element) {
+  element.toggleAttribute('hidden');
+}
+
+// function onInputEnter(element, callback) {
+//   element.addEventListener('keyup', event => {
+//     if (event.key === 'Enter') {
+//       callback(event.target.value);
+//     }
+//   });
+// }
 
 // ================
 // INITIALIZATION
